f226455c5d73880d0599cbaf0175c72da280c8b8
Stefano Monteduro <me@stefanomonteduro.it>
2024-04-09 19:45:48 +0200

    refacot: some refactoring

--- src/Tools/Notice.php
+++ src/Tools/Notice.php
@@ -4,12 +4,17 @@ namespace Awcodes\Scribble\Tools;
 use Awcodes\Scribble\ScribbleTool;
 use Awcodes\Scribble\Enums\ToolType;
 <<<<<<< HEAD
+<<<<<<< HEAD
 use Awcodes\Scribble\Modals\NoticeForm;
 use Closure;
 =======
 use Closure;
 use FilamentMisc\Modals\NoticeForm;
 >>>>>>> 6289081 (feat: customizzazione di scribble)
+=======
+use Awcodes\Scribble\Modals\NoticeForm;
+use Closure;
+>>>>>>> cf8a066 (refacot: some refactoring)
 
 class Notice extends ScribbleTool
 {
