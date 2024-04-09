7d3ebae6f553d03f32d6cee9ae5b1dbcced0d04d
Stefano Monteduro <me@stefanomonteduro.it>
2024-04-09 19:45:48 +0200

    refacot: some refactoring

--- src/Tools/Notice.php
+++ src/Tools/Notice.php
@@ -6,8 +6,11 @@ use Awcodes\Scribble\Enums\ToolType;
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
 >>>>>>> bdae654 (feat: customizzazione di scribble)
+=======
+>>>>>>> 8ce1f90 (refacot: some refactoring)
 use Awcodes\Scribble\Modals\NoticeForm;
 use Closure;
 =======
@@ -21,7 +24,14 @@ use Closure;
 >>>>>>> cf8a066 (refacot: some refactoring)
 =======
 >>>>>>> ec44a0e (feat: customizzazione di scribble)
+<<<<<<< HEAD
 >>>>>>> bdae654 (feat: customizzazione di scribble)
+=======
+=======
+use Awcodes\Scribble\Modals\NoticeForm;
+use Closure;
+>>>>>>> d87b3fd (refacot: some refactoring)
+>>>>>>> 8ce1f90 (refacot: some refactoring)
 
 class Notice extends ScribbleTool
 {
